9b9f1a2d047d4069f9ba3b90b649a68ed95914f6
ImHwaryeon <ghkfus1603@naver.com>
2021-02-17 13:06:42 +0900

    ..

--- js/search.js
+++ js/search.js
@@ -5,10 +5,26 @@ new Vue({
 		titleSub: '이미지 검색',
 		search:'',
 		isSearch: false,
+		products: [],
+		searchProducts: [],
+	},
+	created(){
+		axios.get('../json/products.json')
+		.then((r) => {
+			this.products = r.data;
+			this.searchProducts = this.products;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 	},
 	methods:{
 		onSearchRemove(e){
 			this.search = '';
+			this.searchProducts = this.products;
+		},
+		onSearch(e){
+			this.searchProducts = _.filter(this.products, o => o.title.includes(this.search));
 		}
 	}
 })
