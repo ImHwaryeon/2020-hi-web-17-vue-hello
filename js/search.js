632622f2be77d7153f02d6caf4c57c0151efca9f
ImHwaryeon <ghkfus1603@naver.com>
2021-02-17 11:57:24 +0900

    ..

---
+++ js/search.js
@@ -0,0 +1,14 @@
+new Vue({
+	el: '#app',
+	data: {
+		title: 'Vue | Page형식으로 맛보기',
+		titleSub: '이미지 검색',
+		search:'',
+		isSearch: false,
+	},
+	methods:{
+		onSearchRemove(e){
+			this.search = '';
+		}
+	}
+})
